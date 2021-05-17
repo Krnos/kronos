3730fea7bc889e3b8e483f4c2a8f68737b5a2805
Ivan Sotelo <isotelo@controlla.com.mx>
2021-05-17 13:25:40 -0600

    fix: add module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const getVersions = require('../packages/kronos-get-versions/getVersions')
 const generate = require('../packages/kronos-generate/generate')
 const generateModel = require('../packages/kronos-generate-model/generateModel')
+const checkHasPermissions = require('../packages/kronos-generate-model/checkHasPermissions')
 
 var kronos = {}
 kronos.generate = generate
 kronos.getVersions = getVersions
 kronos.generateModel = generateModel
+kronos.checkHasPermissions = checkHasPermissions
 
 module.exports = kronos
