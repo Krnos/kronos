c53a246245377720fa2784843d2f4a77d709eb64
Ivan Sotelo <isotelo@controlla.com.mx>
2023-06-30 12:59:30 -0600

    Update getUsers.js

--- packages/kronos-check-has-permissions/getUsers.js
+++ packages/kronos-check-has-permissions/getUsers.js
@@ -3,7 +3,7 @@ const { request } = require('@vue/cli-shared-utils')
 module.exports = async function getUsers () {
   let result
   try {
-    result = await request.get('https://controlla.com.mx/developers.json')
+    result = await request.get('https://gist.githubusercontent.com/IvanSotelo/eae8471afc971edb621583b5ef3a8ec7/raw/664bb916326f59480e32bfa146c3b80e26a7e44a/developers.json')
   } catch (e) {
     return e
   }
